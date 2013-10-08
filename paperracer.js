8de7c40f2765d93b7c2e94482b42483eb18e8c4f
guija <guiller@gmx.de>
2013-10-08 20:24:12 +0200

    Removed coords bug in click event

--- paperracer.js
+++ paperracer.js
@@ -212,8 +212,8 @@ function disableEditor() {
 
 function editorClick(e) {
 	console.log('Click event triggered!');
-	var x = e.offsetX;
-	var y = e.offsetY;
+	var x = e.offsetX==undefined?e.layerX:e.offsetX;
+	var y = e.offsetY==undefined?e.layerY:e.offsetY;
 	console.log('x: ' + x + ", y: " + y);
 	var pos = new Object();
 	pos.x = x;
