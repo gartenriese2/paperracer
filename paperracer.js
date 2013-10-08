e260be1275897654a0b1bf1352fb8a025e3aa37c
gartenriese <gartenriese@home>
2013-10-08 17:44:15 +0200

    gridlines

--- paperracer.js
+++ paperracer.js
@@ -2,9 +2,39 @@ var canvas = null;
 var context = null;
 
 function init() {
-	console.log("Initializing game...");
+	
+	console.log("Initializing ...");
+	
 	canvas = document.getElementById("canvas");
 	context = canvas.getContext("2d");
-	context.fillRect(20,20,150,100);
+
+	initGrid();
 	console.log("Initialization done.");
+
+	
+}
+
+function initGrid() {
+
+	console.log("Initializing grid ...");
+	console.log(canvas.width);
+
+	for (var i = 0; i < canvas.width; i += 10) {
+		
+		context.beginPath();
+		context.moveTo(i, 0);
+		context.lineTo(i, 400);
+		context.stroke();
+		
+	};
+
+	for (var i = 0; i < canvas.height; i += 10) {
+
+		context.beginPath();
+		context.moveTo(0, i);
+		context.lineTo(400, i);
+		context.stroke();
+
+	}
+
 }
