efad1bcc045a9180c25e4593352887131cbe2c0d
gartenriese <gartenriese@home>
2013-11-11 21:39:48 +0100

    added movement

--- game.js
+++ game.js
@@ -45,23 +45,53 @@ Game.prototype.click = function(e) {
 						occ == true;
 					}
 				}
-				e.data.game.players[e.data.game.move].setPixelPosition(e.data.game.startingPos[i]);
+				if (!occ) e.data.game.players[e.data.game.move].setPixelPosition(e.data.game.startingPos[i]);
 			}
-			
-			e.data.game.players[e.data.game.move].move(0,0);
 
 		}
 
-		e.data.game.move++;
-		if (e.data.game.move >= e.data.game.players.length) e.data.game.started = true;
+		e.data.game.players[e.data.game.move].move(0,0);
 
 	}
 	// Player moving
 	else {
 
-		e.data.game.move++;
+		var dist = 9999; // todo: better value
+		var count = e.data.game.move % e.data.game.players.length;
+		var player = e.data.game.players[count];
+		var fut = player.getFutures();
+		var futPix = player.getPixelFutures();
+		var oldPos = player.getPosition();
+		var s = new Point(oldPos);
+
+		for (var i = 0; i < fut.length; ++i) {
+
+			var d = point.getDistance(futPix[i]);
+			
+			if (d < dist) {
+				dist = d;
+				var occ = false;
+				for (var j = 0; j < e.data.game.players.length; ++j) {
+					if (e.data.game.players[j].getPosition().getDistance(fut[i]) < 0.1) { // todo: better comparison value
+						occ == true;
+					}
+				}
+				if (!occ) {
+
+					s = new Point(fut[i]);
+					
+				}
+			}
+
+		}
+
+		s.subPoint(oldPos);
+		//s.log();
+		e.data.game.players[count].move(s.x, s.y);
 
 	}
+	e.data.game.move++;
+	if (e.data.game.started == false && e.data.game.move >= e.data.game.players.length) e.data.game.started = true;
 	console.log("Move: " + (e.data.game.move - 1));
 
 	// Refresh the canvas
--- player.js
+++ player.js
@@ -10,6 +10,8 @@ function Player(x, y) {
 Player.prototype.move = function(x, y) {
 	
 	this.position.add(x, y);
+	this.speed.x = x;
+	this.speed.y = y;
 	this.futures = this.calculateFuturePositions();
 
 }
@@ -28,8 +30,8 @@ Player.prototype.setPosition = function(pnt) {
 
 Player.prototype.getPixelPosition = function() {
 	
-	var pos = new Point();
-	pos.addPoint(this.position.mul(gridSize));
+	var pos = new Point(this.position);
+	pos.mul(gridSize);
 	return pos;
 
 }
@@ -41,10 +43,39 @@ Player.prototype.setPixelPosition = function(pnt) {
 
 }
 
+Player.prototype.getSpeed = function() {
+
+	return this.speed;
+	
+}
+
+Player.prototype.getFutures = function() {
+	
+	return this.futures;
+
+}
+
+Player.prototype.getPixelFutures = function() {
+	
+	var arr = [];
+
+	for (var i = 0; i < this.getFutures().length; ++i) {
+
+		var f = new Point(this.getFutures()[i]);
+		f.mul(gridSize);
+		arr.push(f);
+
+	}
+	
+	return arr;
+
+}
+
 Player.prototype.calculateFuturePositions = function() {
 	
 	var pts = [];
 
+	// start
 	if (this.speed.x == 0 && this.speed.y == 0) {
 
 		var p1 = new Point(this.position.x + 1, this.position.y - 1);
@@ -54,6 +85,128 @@ Player.prototype.calculateFuturePositions = function() {
 		pts.push(p2);
 		pts.push(p3);
 
+	} else if (Math.abs(this.speed.x) <= 1 && Math.abs(this.speed.y) <= 1) {
+
+		if (Math.abs(this.speed.x) == 1 && Math.abs(this.speed.y) == 1) {
+
+			var p1 = new Point(this.position.x + this.speed.x, this.position.y + this.speed.y);
+			var p2 = new Point(this.position.x + 2*this.speed.x, this.position.y + 2*this.speed.y);
+			var p3 = new Point(this.position.x + 2*this.speed.x, this.position.y + this.speed.y);
+			var p4 = new Point(this.position.x + this.speed.x, this.position.y + 2*this.speed.y);
+			var p5 = new Point(this.position.x + 2*this.speed.x, this.position.y);
+			var p6 = new Point(this.position.x, this.position.y + 2*this.speed.y);
+			var p7 = new Point(this.position.x + this.speed.x, this.position.y);
+			var p8 = new Point(this.position.x, this.position.y + this.speed.y);
+			pts.push(p1);
+			pts.push(p2);
+			pts.push(p3);
+			pts.push(p4);
+			pts.push(p5);
+			pts.push(p6);
+			pts.push(p7);
+			pts.push(p8);
+
+		} else {
+
+			var p1 = new Point(this.position.x + this.speed.x, this.position.y + this.speed.y);
+			var p2 = new Point(this.position.x + 2*this.speed.x, this.position.y + 2*this.speed.y);
+			pts.push(p1);
+			pts.push(p2);
+
+			if (this.speed.x == 0) {
+				
+				var p3 = new Point(this.position.x + 1, this.position.y + this.speed.y);
+				var p4 = new Point(this.position.x + 1, this.position.y + 2*this.speed.y);
+				var p5 = new Point(this.position.x - 1, this.position.y + this.speed.y);
+				var p6 = new Point(this.position.x - 1, this.position.y + 2*this.speed.y);
+				
+				pts.push(p3);
+				pts.push(p4);
+				pts.push(p5);
+				pts.push(p6);
+
+			} else {
+
+				var p3 = new Point(this.position.x + 2*this.speed.x, this.position.y + 1);
+				var p4 = new Point(this.position.x + this.speed.x, this.position.y + 1);
+				var p5 = new Point(this.position.x + 2*this.speed.x, this.position.y - 1);
+				var p6 = new Point(this.position.x + this.speed.x, this.position.y - 1);
+				
+				pts.push(p3);
+				pts.push(p4);
+				pts.push(p5);
+				pts.push(p6);
+
+			}
+
+		}	
+
+	} else if (Math.abs(this.speed.x) == 5 || Math.abs(this.speed.y) == 5) {
+
+		if (Math.abs(this.speed.x) == 5 && Math.abs(this.speed.y) == 5) {
+
+			var p1 = new Point(this.position.x + this.speed.x, this.position.y + this.speed.y);
+			var p2 = new Point(this.position.x + this.speed.x, this.position.y + this.speed.y - (this.speed.y / 5));
+			var p3 = new Point(this.position.x + this.speed.x - (this.speed.x / 5), this.position.y + this.speed.y);
+			var p4 = new Point(this.position.x + this.speed.x - (this.speed.x / 5), this.position.y + this.speed.y - (this.speed.y / 5));
+			pts.push(p1);
+			pts.push(p2);
+			pts.push(p3);
+			pts.push(p4);
+
+		} else if (Math.abs(this.speed.x) == 5) {
+
+			var p1 = new Point(this.position.x + this.speed.x, this.position.y + this.speed.y);
+			var p2 = new Point(this.position.x + this.speed.x, this.position.y + this.speed.y + 1);
+			var p3 = new Point(this.position.x + this.speed.x, this.position.y + this.speed.y - 1);
+			var p4 = new Point(this.position.x + this.speed.x - (this.speed.x / 5), this.position.y + this.speed.y);
+			var p5 = new Point(this.position.x + this.speed.x - (this.speed.x / 5), this.position.y + this.speed.y + 1);
+			var p6 = new Point(this.position.x + this.speed.x - (this.speed.x / 5), this.position.y + this.speed.y - 1);
+			pts.push(p1);
+			pts.push(p2);
+			pts.push(p3);
+			pts.push(p4);
+			pts.push(p5);
+			pts.push(p6);
+
+		} else {
+
+			var p1 = new Point(this.position.x + this.speed.x, this.position.y + this.speed.y);
+			var p2 = new Point(this.position.x + this.speed.x + 1, this.position.y + this.speed.y);
+			var p3 = new Point(this.position.x + this.speed.x - 1, this.position.y + this.speed.y);
+			var p4 = new Point(this.position.x + this.speed.x, this.position.y + this.speed.y - (this.speed.y / 5));
+			var p5 = new Point(this.position.x + this.speed.x + 1, this.position.y + this.speed.y - (this.speed.y / 5));
+			var p6 = new Point(this.position.x + this.speed.x - 1, this.position.y + this.speed.y - (this.speed.y / 5));
+			pts.push(p1);
+			pts.push(p2);
+			pts.push(p3);
+			pts.push(p4);
+			pts.push(p5);
+			pts.push(p6);
+
+		}
+
+	} else {
+
+		var p1 = new Point(this.position.x + this.speed.x, this.position.y + this.speed.y);
+		var p2 = new Point(this.position.x + this.speed.x, this.position.y + this.speed.y + 1);
+		var p3 = new Point(this.position.x + this.speed.x, this.position.y + this.speed.y - 1);
+		var p4 = new Point(this.position.x + this.speed.x + 1, this.position.y + this.speed.y);
+		var p5 = new Point(this.position.x + this.speed.x + 1, this.position.y + this.speed.y + 1);
+		var p6 = new Point(this.position.x + this.speed.x + 1, this.position.y + this.speed.y - 1);
+		var p7 = new Point(this.position.x + this.speed.x - 1, this.position.y + this.speed.y);
+		var p8 = new Point(this.position.x + this.speed.x - 1, this.position.y + this.speed.y + 1);
+		var p9 = new Point(this.position.x + this.speed.x - 1, this.position.y + this.speed.y - 1);
+		pts.push(p1);
+		pts.push(p2);
+		pts.push(p3);
+		pts.push(p4);
+		pts.push(p5);
+		pts.push(p6);
+		pts.push(p7);
+		pts.push(p8);
+		pts.push(p9);
+		
 	}
 
 	return pts;
@@ -72,8 +225,8 @@ Player.prototype.drawFutures = function() {
 	
 	for (var i = 0; i < this.futures.length; ++i) {
 
-		var pixFut = this.futures[i].mul(gridSize);
-
+		var pixFut = new Point(this.futures[i]);
+		pixFut.mul(gridSize);
 		pixFut.draw(context, "#ff0000");
 
 	}
